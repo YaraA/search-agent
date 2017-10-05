5d3e2770621c6934f629101877ae9be904991417
Hagar Yasser <hagar.y.omar@gmail.com>
2017-10-05 22:09:45 +0200

    removed cell types classes and removed abstract declaration of Cell class #20

--- search-agent/src/Cell.java
+++ search-agent/src/Cell.java
@@ -15,11 +15,4 @@ public class Cell {
 	public void setType(CellType type) {
 		this.type = type;
 	}
-
-	public boolean canMoveThrough(){
-		return false;
-	}
-	//overwritten in each of the Cell's subclasses
-	//checks whether R2-D2 can pass through the cell or not
-
 }
