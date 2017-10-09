8c5ba0b0a0097a400c9a38aa03eba5ab65428fd9
Hagar Mosaad <hagary@users.noreply.github.com>
2017-10-09 18:18:28 +0300

    Update State.java

--- search-agent/src/State.java
+++ search-agent/src/State.java
@@ -2,13 +2,4 @@
 public class State {
 	public State() {	
 	}
-	public Grid getCurrGrid() {
-		return currGrid;
-	}
-	public int getCurrPosRow() {
-		return currPosRow;
-	}
-	public int getCurrPosCol() {
-		return currPosCol;
-	}
 }
